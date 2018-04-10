eb84d0f4c706d04109e50f168efbdc6b1756ea5c
Abhijit Kamat <avk41+blah@scarletmail.rutgers.edu>
2018-04-10 10:51:08 -0400

    concept for reading data

--- persist/persist.py
+++ persist/persist.py
@@ -1,17 +1,5 @@
 from pymongo import MongoClient
-import requests
-
-#name_brief doesn't work without first and last name.
-def names_exist(player):
-    return not player['firstname'] or not player['lastname']
-
-#return format for name brief for specific sport
-def get_name_brief(sport, player):
-    firstname = player['firstname']
-    lastname = player['lastname']
-    if(sport == 'baseball'):    return firstname[0] + '. ' + lastname[0] + '.'
-    if(sport == 'basketball'):  return firstname +' ' + lastname[0] + '.'
-    if(sport == 'football'):    return firstname[0] + '. ' + lastname
+import requests, utils
 
 client = MongoClient() 
 sports = ['baseball', 'basketball', 'football']
@@ -19,17 +7,18 @@ sports = ['baseball', 'basketball', 'football']
 for sport in sports:
     r = requests.get(url = 'http://api.cbssports.com/fantasy/players/list', params = {'version':'3.0', 'SPORT': sport, 'response_format':'JSON'})
     players = r.json()['body']['players']
-
+    print(sport.upper()+'\n')
     for player in players:
-        if not names_exist(player): continue 
+        if not utils.vars_exist(player): continue 
         
         payload = {
             'id': player['id'],
-            'name_brief':   get_name_brief(),
+            'name_brief':   utils.get_name_brief(sport,player),
             'first_name':   player['firstname'],
             'last_name':    player['lastname'],
             'position':     player['position'],
             'age':          player['age']
         }
 
+        print(payload)
     
