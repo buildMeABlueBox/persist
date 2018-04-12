f6ce7ff4c91e2414aadb6606572c5019322635ea
Abhijit Kamat <avk41+blah@scarletmail.rutgers.edu>
2018-04-12 08:56:20 -0400

    dot notation

--- persist/persist.py
+++ persist/persist.py
@@ -16,12 +16,22 @@ for sport in sports:
         if not utils.vars_exist(player): continue 
 
         player_info = {
-            'id': player['id'],
+            'id': player.id,
             'name_brief':   utils.get_name_brief(sport,player),
-            'first_name':   player['firstname'],
-            'last_name':    player['lastname'],
-            'position':     player['position'],
-            'age':          player['age']
+            'first_name':   player.firstname,
+            'last_name':    player.lastname,
+            'position':     player.position,
+            'age':          player.age
+        }
+
+        initial_position_info = {
+            'position_info': {
+                'position': player.position,
+                'players': 1,
+                'age_total': player.age,
+                'age_avg' : player.age
+            }
         }
 
         utils.insert(collection, player_info)
+        utils.insert(collection, initial_position_info)
